def870a313425298326780f3277ad20bb9725274
Leonardo Nunes <zleo465@gmail.com>
2022-01-24 19:12:23 -0200

    Created SectionTitle component

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 import HeaderFashion from "./src/components/HeaderFashion";
+import SectionTitle from "./src/components/SectionTitle";
+
 import "./src/styles/generic/reset.css";
+import "./style.css"
 const $root = document.querySelector('#app');
-$root.insertAdjacentHTML("beforeend", HeaderFashion())
+$root.insertAdjacentHTML("beforeend", `
+
+${HeaderFashion()}
+${SectionTitle()}
+`)
 
---
+++ src/components/SectionTitle/index.js
@@ -0,0 +1,24 @@
+const SectionTitle = () =>{
+    return/*html*/`
+    <section>
+
+    <div class="computer-div"> </div>
+
+    <div class="div-title">
+    <h1>Receba ofertas e descontos exclusivos</h1>
+    <h3>Na Fashion, você vai encontrar roupas de todos os tamanhos e estilos para homens e mulheres. Se a sua pegada for mais básica, nós temos camisetas da melhor qualidade. Agora, se você preferir de roupas mais sofisticadas, contamos com as principais grifes do mercado.</h3>
+    <button class="universal-button">Leia mais ></button>
+
+    <ul class="circle-list">
+    <li> </li>
+    <li> </li>
+    <li> </li>
+    <li> </li>
+    <li> </li>
+    </ul>
+
+    </div>
+    </section>
+    `
+}
+export default SectionTitle
